b60b76624af5e469379151e70dda7c41222b0b77
taewoo <qkqnrpa@gmail.com>
2022-07-05 15:20:20 +0900

    [refactor] Book 모델에서 사용하지 않는 값 제거

--- Wanted-Mobile-Test/Source/Model/Book.swift
+++ Wanted-Mobile-Test/Source/Model/Book.swift
@@ -6,91 +6,26 @@
 //
 
 
-struct Book: Decodable, Equatable {
-//    let kind: String
+struct Book: Decodable {
     let id: String
-//    let etag: String
-//    let selfLink: String
     let volumeInfo: Info
-//    let saleInfo: SaleInfo
-//    let accessInfo: AccessInfo
-//    let searchInfo : SearchInfo?
-    
-    static func == (lhs: Book, rhs: Book) -> Bool {
-        return lhs.id == rhs.id
-    }
-    
     
     struct Info: Decodable {
         let title: String
         let subtitle: String?
         let authors: [String]?
         let publishedDate: String?
-//        let industryIdentifiers: [IndustryID]
-//        let readingModes: ReadingModes
-//        let pageCount: Int?
-//        let printType: String
-//        let categories: [String]?
-//        let maturityRating: String
-//        let allowAnonLogging: Bool
-//        let contentVersion: String
-//        let panelizationSummary: PanelizationSummary
         let imageLinks: ImageLinks?
-//        let language: String
-//        let previewLink: String
-//        let infoLink: String
-//        let canonicalVolumeLink: String
-        
-        struct IndustryID: Decodable {
-            let type: String
-            let identifier: String
-        }
-        
-        struct ReadingModes: Decodable {
-            let text: Bool
-            let image: Bool
-        }
-        
-        struct PanelizationSummary: Decodable {
-            let containsEpubBubbles: Bool
-            let containsImageBubbles: Bool
-        }
         
         struct ImageLinks: Decodable {
             let smallThumbnail: String
             let thumbnail: String
         }
     }
-    
-    struct SaleInfo: Decodable {
-        let country: String
-        let saleability: String
-        let isEbook: Bool
-    }
-    
-    struct AccessInfo: Decodable {
-        let country: String
-        let viewability: String
-        let embeddable: Bool
-        let publicDomain: Bool
-        let textToSpeechPermission: String
-        let epub: Epub
-        let pdf: PDF
-        let webReaderLink: String
-        let accessViewStatus: String
-        let quoteSharingAllowed: Bool
-        
-        struct Epub: Decodable {
-            let isAvailable: Bool
-        }
-        
-        struct PDF: Decodable {
-            let isAvailable: Bool
-        }
-    }
-    
-    struct SearchInfo: Decodable {
-        let textSnippet: String
-    }
+}
 
+extension Book: Equatable {
+    static func == (lhs: Book, rhs: Book) -> Bool {
+        return lhs.id == rhs.id
+    }
 }
